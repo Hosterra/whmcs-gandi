dcf1387efbcbb92668962002d4854f4ac0d36ca6
Pierre Lannoy <pierre@lannoy.fr>
2022-10-26 16:29:51 +0200

    Adds additional fields references for domains.

--- resources/domains/additionalfields.php
+++ resources/domains/additionalfields.php
@@ -1,33 +1,80 @@
 <?php
 /*
  **********************************************************************
- *         Additional Domain Fields for Gandi                         *
+ *         Reconfigure Additional Domain Fields for Gandi             *
  **********************************************************************
  */
 
-// .FR
-unset( $additionaldomainfields[".fr"] );
-$additionaldomainfields[".fr"][] = array("Name" => "Legal Type", "LangVar" => "fr_legaltype", "Type" => "dropdown", "Options" => "Individual,Company", "Default" => "Individual",);
-$additionaldomainfields[".fr"][] = array("Name" => "Info", "LangVar" => "fr_info", "Type" => "display", "Default" =>
-    "{$_LANG['enomfrregistration']['Heading']}
-        <ul>
-            <li><strong>{$_LANG['enomfrregistration']['French Individuals']['Name']}</strong>: {$_LANG['enomfrregistration']['French Individuals']['Requirements']}</li>
-            <li><strong>{$_LANG['enomfrregistration']['EU Non-French Individuals']['Name']}</strong>: {$_LANG['enomfrregistration']['EU Non-French Individuals']['Requirements']}</li>
-            <li><strong>{$_LANG['enomfrregistration']['French Companies']['Name']}</strong>: {$_LANG['enomfrregistration']['French Companies']['Requirements']}</li>
-            <li><strong>{$_LANG['enomfrregistration']['EU Non-French Companies']['Name']}</strong>: {$_LANG['enomfrregistration']['EU Non-French Companies']['Requirements']}</li>
-        </ul>
-        <em>{$_LANG['enomfrregistration']['Non-EU Warning']}</em>",);
-$additionaldomainfields[".fr"][] = array("Name" => "Birthdate", 'LangVar' => 'fr_indbirthdate', "Type" => "text","Size" => "16","Default" => "1900-01-01","Required" => false);
-$additionaldomainfields[".fr"][] = array("Name" => "Birthplace City", 'LangVar' => 'fr_indbirthcity', "Type" => "text","Size" => "25","Default" => "","Required" => false);
-$additionaldomainfields[".fr"][] = array("Name" => "Birthplace Country", 'LangVar' => 'fr_indbirthcountry', "Type" => "text", "Size" => "2", "Default" => "", "Required" => false, "Description" => "Please enter your country code (eg. FR, IT, etc...)");
-$additionaldomainfields[".fr"][] = array("Name" => "Birthplace Postcode", 'LangVar' => 'fr_indbirthpostcode', "Type" => "text","Size" => "6","Default" => "","Required" => false);
-$additionaldomainfields[".fr"][] = array("Name" => "SIRET Number", 'LangVar' => 'fr_cosiret', "Type" => "text","Size" => "50","Default" => "","Required" => false);
-$additionaldomainfields[".fr"][] = array("Name" => "DUNS Number", 'LangVar' => 'fr_coduns', "Type" => "text","Size" => "50","Default" => "","Required" => false);
-$additionaldomainfields[".fr"][] = array("Name" => "VAT Number", 'LangVar' => 'fr_vat', "Type" => "text","Size" => "50","Default" => "","Required" => false);
-$additionaldomainfields[".fr"][] = array("Name" => "Trademark Number", 'LangVar' => 'fr_trademarknumber', "Type" => "text","Size" => "50","Default" => "","Required" => false);
+// .AERO
+$additionaldomainfields['.aero'][] = array('Name' => '.AERO ID', "Remove" => true);
+$additionaldomainfields['.aero'][] = array('Name' => '.AERO Key', "Remove" => true);
+$additionaldomainfields['.aero'][] = array('Name' => 'x-aero_ens_authid', "LangVar" => "aeroid", 'DisplayName' => '.AERO ID <sup style="cursor:help;" title="Obtain from http://www.information.aero/">what\'s this?</sup>', 'Type' => 'text', 'Size' => '20', 'Required' => true);
+$additionaldomainfields['.aero'][] = array('Name' => 'x-aero_ens_authkey', "LangVar" => "aerokey", 'DisplayName'	=> '.AERO Key <sup style="cursor:help;" title="Obtain from http://www.information.aero/">what\'s this?</sup>', 'Type' => 'text', 'Size' => '20');
 
-$additionaldomainfields[".re"] = $additionaldomainfields[".fr"];
-$additionaldomainfields[".pm"] = $additionaldomainfields[".fr"];
-$additionaldomainfields[".tf"] = $additionaldomainfields[".fr"];
-$additionaldomainfields[".wf"] = $additionaldomainfields[".fr"];
-$additionaldomainfields[".yt"] = $additionaldomainfields[".fr"];
+// .FR
+$additionaldomainfields[".fr"][] = array("Name" => "Legal Type", "Remove" => true);
+$additionaldomainfields[".fr"][] = array("Name" => "Info", "Remove" => true);
+$additionaldomainfields[".fr"][] = array("Name" => "Birthdate", "Remove" => true);
+$additionaldomainfields[".fr"][] = array("Name" => "Birthplace City", "Remove" => true);
+$additionaldomainfields[".fr"][] = array("Name" => "Birthplace Country", "Remove" => true);
+$additionaldomainfields[".fr"][] = array("Name" => "Birthplace Postcode", "Remove" => true);
+$additionaldomainfields[".fr"][] = array("Name" => "SIRET Number", "Remove" => true);
+$additionaldomainfields[".fr"][] = array("Name" => "DUNS Number", "Remove" => true);
+$additionaldomainfields[".fr"][] = array("Name" => "VAT Number", "Remove" => true);
+$additionaldomainfields[".fr"][] = array("Name" => "Trademark Number", "Remove" => true);
+
+$additionaldomainfields[".re"][] = array("Name" => "Legal Type", "Remove" => true);
+$additionaldomainfields[".re"][] = array("Name" => "Info", "Remove" => true);
+$additionaldomainfields[".re"][] = array("Name" => "Birthdate", "Remove" => true);
+$additionaldomainfields[".re"][] = array("Name" => "Birthplace City", "Remove" => true);
+$additionaldomainfields[".re"][] = array("Name" => "Birthplace Country", "Remove" => true);
+$additionaldomainfields[".re"][] = array("Name" => "Birthplace Postcode", "Remove" => true);
+$additionaldomainfields[".re"][] = array("Name" => "SIRET Number", "Remove" => true);
+$additionaldomainfields[".re"][] = array("Name" => "DUNS Number", "Remove" => true);
+$additionaldomainfields[".re"][] = array("Name" => "VAT Number", "Remove" => true);
+$additionaldomainfields[".re"][] = array("Name" => "Trademark Number", "Remove" => true);
+
+$additionaldomainfields[".pm"][] = array("Name" => "Legal Type", "Remove" => true);
+$additionaldomainfields[".pm"][] = array("Name" => "Info", "Remove" => true);
+$additionaldomainfields[".pm"][] = array("Name" => "Birthdate", "Remove" => true);
+$additionaldomainfields[".pm"][] = array("Name" => "Birthplace City", "Remove" => true);
+$additionaldomainfields[".pm"][] = array("Name" => "Birthplace Country", "Remove" => true);
+$additionaldomainfields[".pm"][] = array("Name" => "Birthplace Postcode", "Remove" => true);
+$additionaldomainfields[".pm"][] = array("Name" => "SIRET Number", "Remove" => true);
+$additionaldomainfields[".pm"][] = array("Name" => "DUNS Number", "Remove" => true);
+$additionaldomainfields[".pm"][] = array("Name" => "VAT Number", "Remove" => true);
+$additionaldomainfields[".pm"][] = array("Name" => "Trademark Number", "Remove" => true);
+
+$additionaldomainfields[".tf"][] = array("Name" => "Legal Type", "Remove" => true);
+$additionaldomainfields[".tf"][] = array("Name" => "Info", "Remove" => true);
+$additionaldomainfields[".tf"][] = array("Name" => "Birthdate", "Remove" => true);
+$additionaldomainfields[".tf"][] = array("Name" => "Birthplace City", "Remove" => true);
+$additionaldomainfields[".tf"][] = array("Name" => "Birthplace Country", "Remove" => true);
+$additionaldomainfields[".tf"][] = array("Name" => "Birthplace Postcode", "Remove" => true);
+$additionaldomainfields[".tf"][] = array("Name" => "SIRET Number", "Remove" => true);
+$additionaldomainfields[".tf"][] = array("Name" => "DUNS Number", "Remove" => true);
+$additionaldomainfields[".tf"][] = array("Name" => "VAT Number", "Remove" => true);
+$additionaldomainfields[".tf"][] = array("Name" => "Trademark Number", "Remove" => true);
+
+$additionaldomainfields[".wf"][] = array("Name" => "Legal Type", "Remove" => true);
+$additionaldomainfields[".wf"][] = array("Name" => "Info", "Remove" => true);
+$additionaldomainfields[".wf"][] = array("Name" => "Birthdate", "Remove" => true);
+$additionaldomainfields[".wf"][] = array("Name" => "Birthplace City", "Remove" => true);
+$additionaldomainfields[".wf"][] = array("Name" => "Birthplace Country", "Remove" => true);
+$additionaldomainfields[".wf"][] = array("Name" => "Birthplace Postcode", "Remove" => true);
+$additionaldomainfields[".wf"][] = array("Name" => "SIRET Number", "Remove" => true);
+$additionaldomainfields[".wf"][] = array("Name" => "DUNS Number", "Remove" => true);
+$additionaldomainfields[".wf"][] = array("Name" => "VAT Number", "Remove" => true);
+$additionaldomainfields[".wf"][] = array("Name" => "Trademark Number", "Remove" => true);
+
+$additionaldomainfields[".yt"][] = array("Name" => "Legal Type", "Remove" => true);
+$additionaldomainfields[".yt"][] = array("Name" => "Info", "Remove" => true);
+$additionaldomainfields[".yt"][] = array("Name" => "Birthdate", "Remove" => true);
+$additionaldomainfields[".yt"][] = array("Name" => "Birthplace City", "Remove" => true);
+$additionaldomainfields[".yt"][] = array("Name" => "Birthplace Country", "Remove" => true);
+$additionaldomainfields[".yt"][] = array("Name" => "Birthplace Postcode", "Remove" => true);
+$additionaldomainfields[".yt"][] = array("Name" => "SIRET Number", "Remove" => true);
+$additionaldomainfields[".yt"][] = array("Name" => "DUNS Number", "Remove" => true);
+$additionaldomainfields[".yt"][] = array("Name" => "VAT Number", "Remove" => true);
+$additionaldomainfields[".yt"][] = array("Name" => "Trademark Number", "Remove" => true);
+
