2db7e7a1e5a716b0e18e087e96a51ad7edca8df7
Pierre Lannoy <pierre@lannoy.fr>
2022-11-09 00:37:09 +0100

    Precise domain fields.

--- lib/SpecialFields.php
+++ lib/SpecialFields.php
@@ -12,11 +12,11 @@ class SpecialFields {
 	    'x-aero_ens_authkey',
 	    'x-au_registrant_id_number',
 	    'x-au_registrant_id_type',
-	    'x-az_admin_pp_id',
-	    'x-az_billing_pp_id',
-	    'x-az_registrant_company_number',
-	    'x-az_registrant_pp_id',
-	    'x-az_tech_pp_id',
+	    // gandi corporate => 'x-az_admin_pp_id',
+	    // gandi corporate => 'x-az_billing_pp_id',
+	    // gandi corporate => 'x-az_registrant_company_number',
+	    // gandi corporate => 'x-az_registrant_pp_id',
+	    // gandi corporate => 'x-az_tech_pp_id',
 	    'x-barcelona_intendeduse',
 	    'x-barcelona_trademarkid',
 	    'x-ca_business_entity_type',
@@ -26,79 +26,79 @@ class SpecialFields {
 	    // deprecated => 'x-ca_owner_name',
 	    'x-cat_intendeduse',
 	    // deprecated => 'x-coop_sponsor',
-	    'x-cy_reg_identity_no',
+	    // gandi corporate => 'x-cy_reg_identity_no',
 	    'x-document_country',
 	    'x-document_type',
 	    'x-document_value',
-	    'x-es_admin_identification',
-	    'x-es_admin_tipo_identification',
-	    'x-es_bill_identification',
-	    'x-es_bill_tipo_identification',
+	    'x-es_admin_identification',                // Excluded
+	    'x-es_admin_tipo_identification',           // Excluded
+	    'x-es_bill_identification',                 // Excluded
+	    'x-es_bill_tipo_identification',            // Excluded
 	    // deprecated => 'x-es_identification',
 	    // deprecated => 'x-es_legalform',
-	    'x-es_owner_identification',
-	    'x-es_owner_legalform',
-	    'x-es_owner_tipo_identification',
-	    'x-es_tech_identification',
-	    'x-es_tech_tipo_identification',
+	    'x-es_owner_identification',                // Excluded
+	    'x-es_owner_legalform',                     // Excluded
+	    'x-es_owner_tipo_identification',           // Excluded
+	    'x-es_tech_identification',                 // Excluded
+	    'x-es_tech_tipo_identification',            // Excluded
 	    // deprecated => 'x-es_tipo_identification',
 	    'x-eu_country_of_citizenship',
 	    'x-eus_intendeduse',
-	    'x-fi_business_number',
-	    'x-fi_ident_number',
-	    'x-fi_isfinish',
-	    'x-fi_organization_type',
+	    'x-fi_business_number',                     // Excluded
+	    'x-fi_ident_number',                        // Excluded
+	    'x-fi_isfinish',                            // Excluded
+	    'x-fi_organization_type',                   // Excluded
 	    'x-gal_intendeduse',
-	    'x-hk_registrant_birth_date',
-	    'x-hk_registrant_document_number',
-	    'x-hk_registrant_document_origin_country',
-	    'x-hk_registrant_document_type',
-	    'x-hk_registrant_other_document_type',
-	    'x-hr_ident_number',
-	    'x-hr_non_eu_pm_business_number',
-	    'x-hu_idnumber',
-	    'x-hu_owner_vatid',
-	    'x-ie_registrant_cro_number',
-	    'x-ie_registrant_supporting_number',
-	    'x-ie_registrant_type',
-	    'x-il_registrant_company_type',
-	    'x-it_nationality',
-	    'x-it_pin',
-	    'x-it_registrant_entity_type',
-	    // deprecated => 'x-jobs_website',
-	    'x-kr_reg_identity_no',
+	    'x-hk_registrant_birth_date',               // Excluded
+	    'x-hk_registrant_document_number',          // Excluded
+	    'x-hk_registrant_document_origin_country',  // Excluded
+	    'x-hk_registrant_document_type',            // Excluded
+	    'x-hk_registrant_other_document_type',      // Excluded
+	    // gandi corporate => 'x-hr_ident_number',
+	    // gandi corporate => 'x-hr_non_eu_pm_business_number',
+	    // gandi corporate => 'x-hu_idnumber',
+	    // gandi corporate => 'x-hu_owner_vatid',
+	    'x-ie_registrant_cro_number',               // Excluded
+	    'x-ie_registrant_supporting_number',        // Excluded
+	    'x-ie_registrant_type',                     // Excluded
+	    // gandi corporate => 'x-il_registrant_company_type',
+	    'x-it_nationality',                         // Excluded
+	    'x-it_pin',                                 // Excluded
+	    'x-it_registrant_entity_type',              // Excluded
+	    'x-jobs_website',
+	    // gandi corporate => 'x-kr_reg_identity_no',
 	    'x-lt_registrant_legal_id',
 	    'x-ltda_authority',
 	    'x-ltda_licensenumber',
-	    'x-lv_idnumber',
+	    // deprecated => 'x-lv_idnumber',
 	    'x-madrid_intendeduse',
-	    'x-my_admin_contact_password',
-	    'x-my_admin_contact_username',
-	    'x-my_business_number',
-	    'x-my_organization_type',
-	    'x-no_registrant_identity',
+	    'x-my_admin_contact_password',              // Excluded
+	    'x-my_admin_contact_username',              // Excluded
+	    'x-my_business_number',                     // Excluded
+	    'x-my_organization_type',                   // Excluded
+	    'x-no_registrant_identity',                 // Excluded
 	    'x-nu_registrant_idnumber',
 	    'x-nu_registrant_vatid',
-	    'x-nyc_extcontact',
-	    'x-pro_authority',
-	    'x-pro_authoritywebsite',
-	    'x-pro_licensenumber',
-	    'x-pro_profession',
-	    'x-promopixel_company_serial',
-	    'x-pt_arbitration',
-	    'x-pt_registrant_vatid',
-	    'x-pt_roid',
-	    'x-pt_tech_vatid',
+	    'x-nyc_extcontact',                         // Excluded
+	    'x-pro_authority',                          // Excluded
+	    'x-pro_authoritywebsite',                   // Excluded
+	    'x-pro_licensenumber',                      // Excluded
+	    'x-pro_profession',                         // Excluded
+	    // What the hell is that??? => 'x-promopixel_company_serial',
+	    'x-pt_arbitration',                 //todo
+	    'x-pt_registrant_vatid',            //todo
+	    'x-pt_roid',                        //todo
+	    'x-pt_tech_vatid',                  //todo
 	    'x-quebec_intendeduse',
 	    'x-radio_intendeduse',
 	    'x-ro_registrant_idnumber',
-	    'x-rs_company_number',
-	    'x-rs_reg_identity_no',
-	    'x-rs_reg_tax_no',
-	    'x-ru_registrant_birth_date',
-	    'x-ru_registrant_kpp',
-	    'x-ru_registrant_passport_data',
-	    'x-ru_registrant_tin',
+	    // gandi corporate => 'x-rs_company_number',
+	    // gandi corporate => 'x-rs_reg_identity_no',
+	    // gandi corporate => 'x-rs_reg_tax_no',
+	    // deprecated => 'x-ru_registrant_birth_date',
+	    // deprecated => 'x-ru_registrant_kpp',
+	    // deprecated => 'x-ru_registrant_passport_data',
+	    // deprecated => 'x-ru_registrant_tin',
 	    'x-scot_intendeduse',
 	    'x-se_ident_number',
 	    'x-se_registrant_vatid',
@@ -114,8 +114,8 @@ class SpecialFields {
 	    'x-uk_contact_type',
 	    'x-us_app_purpose',
 	    'x-us_nexus_category',
-	    'x-xxx_membership_contact',
-	    'x-xxx_sponsored_community',
+	    'x-xxx_membership_contact',                 // Excluded
+	    'x-xxx_sponsored_community',                // Excluded
     ];
 
 	/*
@@ -133,11 +133,11 @@ class SpecialFields {
 		'x-aero_ens_authkey',
 		'x-au_registrant_id_number',
 		'x-au_registrant_id_type',
-		'x-az_admin_pp_id',
-		'x-az_billing_pp_id',
-		'x-az_registrant_company_number',
-		'x-az_registrant_pp_id',
-		'x-az_tech_pp_id',
+		// gandi corporate => 'x-az_admin_pp_id',
+		// gandi corporate => 'x-az_billing_pp_id',
+		// gandi corporate => 'x-az_registrant_company_number',
+		// gandi corporate => 'x-az_registrant_pp_id',
+		// gandi corporate => 'x-az_tech_pp_id',
 		'x-barcelona_intendeduse',
 		'x-barcelona_trademarkid',
 		'x-ca_business_entity_type',
@@ -147,7 +147,7 @@ class SpecialFields {
 		// deprecated => 'x-ca_owner_name',
 		'x-cat_intendeduse',
 		// deprecated => 'x-coop_sponsor',
-		'x-cy_reg_identity_no',
+		// gandi corporate => 'x-cy_reg_identity_no',
 		'x-document_country',
 		'x-document_type',
 		'x-document_value',
