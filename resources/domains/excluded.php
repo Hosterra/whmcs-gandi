33f9bf80296a65988a1d639bc92c895ffa6b4e9c
Pierre Lannoy <pierre@lannoy.fr>
2022-11-09 00:36:13 +0100

    Adds some TLDs.

--- resources/domains/excluded.php
+++ resources/domains/excluded.php
@@ -2,4 +2,4 @@
 
 // THESE TLDS ARE NOT REGISTRABLE
 
-$excluded_tlds = [ '.es', '.hk' ];
+$excluded_tlds = [ '.es', '.fi', '.hk', '.ie', '.it', '.my', '.no', '.nyc', '.pt', '.ru', '.xn--p1ai', '.xxx' ];
