f23fcc8cb318b23262b1dcf224b564785c81f52a
Pierre Lannoy <pierre@lannoy.fr>
2022-11-09 12:39:29 +0100

    Updates domains list.

--- resources/domains/corporateservices.php
+++ resources/domains/corporateservices.php
@@ -2,4 +2,6 @@
 
 // THESE TLDS ARE NOT REGISTRABLE
 
-$corporate_tlds = [];
+// Corporate services reserved domains - Checked 2022-11-09
+
+$corporate_tlds = [ '.ad', '.ae', '.al', '.ao', '.aq', '.ar', '.au', '.ax', '.az', '.ba', '.bank', '.bb', '.bd', '.bf', '.bg', '.bh', '.bi', '.bj', '.bm', '.bn', '.bo', '.br', '.bs', '.bw', '.by', '.bt', '.biz.ss', '.cd', '.cg', '.ci', '.ck', '.cl', '.cr', '.cu', '.cw', '.cy', '.cf', '.com.ss', '.co.jp', '.dj', '.dm', '.do', '.dz', '.ec', '.ee', '.eg', '.et', '.fj', '.fo', '.ga', '.ge', '.gf', '.gh', '.gi', '.gm', '.gn', '.gp', '.gq', '.gt', '.gw', '.hm', '.hr', '.hu', '.id', '.il', '.insurance', '.iq', '.ir', '.is', '.jm', '.jo', '.ke', '.kg', '.kn', '.kr', '.kw', '.ky', '.kz', '.kh', '.lb', '.lk', '.ls', '.ly', '.lr', '.ma', '.mc', '.md', '.mk', '.ml', '.mm', '.mo', '.mp', '.mq', '.mr', '.mt', '.mw', '.my', '.mz', '.mv', '.me.ss', '.nc', '.ne', '.ng', '.ni', '.np', '.nr', '.net.ss', '.om', '.pa', '.pf', '.pg', '.pk', '.pn', '.ps', '.py', '.qa', '.rs', '.rw', '.sa', '.sd', '.si', '.sl', '.sm', '.sn', '.sr', '.su', '.sv', '.sy', '.td', '.tg', '.th', '.tj', '.tk', '.tn', '.to', '.tr', '.tt', '.tz', '.ua', '.ug', '.uy', '.uz', '.ve', '.vi', '.vn', '.vu', '.ye', '.zm', 'zw' ];
