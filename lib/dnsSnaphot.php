8f60b3bf19293bb7b80738be6908a7fc27c19ebe
Pierre Lannoy <pierre@lannoy.fr>
2022-12-15 14:22:27 +0100

    Adds restoring capability.

--- lib/dnsSnaphot.php
+++ lib/dnsSnaphot.php
@@ -11,12 +11,31 @@ class dnsSnaphot {
 	private $apiKey;
 	private $livednsApi;
 	private $snapshots = null;
+	private $details = null;
+	private static $instance = null;
 
+	public static function instance( $apiKey, $domain ) {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new self( $apiKey, $domain );
+		}
+
+		return self::$instance;
+	}
 
 	public function __construct( $apiKey, $domain ) {
 		$this->apiKey     = $apiKey;
 		$this->domain     = $domain;
 		$this->livednsApi = new LiveDNS( $this->apiKey );
+	}
+
+	/*
+	*
+	* Reset status and invalidate cache.
+	*
+	*/
+	public function reset() {
+		$this->snapshots = null;
+		$this->details   = null;
 	}
 
 	/*
@@ -71,35 +90,40 @@ class dnsSnaphot {
 	*
 	*/
 	public function getDetails( $id = '' ) {
-		$result = [
-			'id'   => $id,
-			'name' => '-',
-			'date' => '-',
-			'status' => 'error',
-			'data' => [],
-		];
-		if ( ! $this->isValid( $id ) ) {
-			return $result;
-		}
-		$response = $this->livednsApi->getSnapshotDetails( $this->domain, $id );
-		if ( ( isset( $response->code ) && 200 !== (int) $response->code ) ) {
-			return $result;
-		}
-		$snap = (array) $response;
-		$result['name']   = $snap['name'] ?? '-';
-		if ( array_key_exists( 'automatic', $snap ) ) {
-			$result['status'] = $snap['automatic'] ? 'auto' : 'manual';
-		}
-		if ( array_key_exists( 'created_at', $snap ) ) {
-			$result['date'] = Carbon::parse( $snap['created_at'] )->format( 'Y-m-d H:m:i' );
-		}
-		if ( array_key_exists( 'zone_data', $snap ) ) {
-			foreach ( $snap['zone_data'] as $s ) {
-				$result['data'][] = (array) $s;
+		if ( ! isset( $this->details ) ) {
+			$result = [
+				'id'        => $id,
+				'name'      => '-',
+				'date'      => '-',
+				'status'    => 'error',
+				'data'      => [],
+				'zone_data' => null,
+			];
+			if ( ! $this->isValid( $id ) ) {
+				return $result;
 			}
+			$response = $this->livednsApi->getSnapshotDetails( $this->domain, $id );
+			if ( ( isset( $response->code ) && 200 !== (int) $response->code ) ) {
+				return $result;
+			}
+			$snap           = (array) $response;
+			$result['name'] = $snap['name'] ?? '-';
+			if ( array_key_exists( 'automatic', $snap ) ) {
+				$result['status'] = $snap['automatic'] ? 'auto' : 'manual';
+			}
+			if ( array_key_exists( 'created_at', $snap ) ) {
+				$result['date'] = Carbon::parse( $snap['created_at'] )->format( 'Y-m-d H:m:i' );
+			}
+			if ( array_key_exists( 'zone_data', $snap ) ) {
+				$result['zone_data'] = $snap['zone_data'];
+				foreach ( $snap['zone_data'] as $s ) {
+					$result['data'][] = (array) $s;
+				}
+			}
+			$this->details = $result;
 		}
 
-		return $result;
+		return $this->details;
 	}
 
 	/*
@@ -112,6 +136,39 @@ class dnsSnaphot {
 	public function takeNow( $name = '' ) {
 		$response = $this->livednsApi->createSnapshot( $this->domain, $name );
 		if ( isset( $response->code ) && 200 !== (int) $response->code ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/*
+	*
+	*Delete a snapshot.
+	*
+	* @return bool
+	*
+	*/
+	public function deleteNow( $id = '' ) {
+		$response = $this->livednsApi->deleteSnapshot( $this->domain, $id );
+		if ( isset( $response->code ) && 204 !== (int) $response->code ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/*
+	*
+	*Restore a snapshot.
+	*
+	* @return bool
+	*
+	*/
+	public function restoreNow( $id = '' ) {
+		$this->getDetails( $id );
+		$response = $this->livednsApi->restoreSnapshot( $this->domain, $this->details['zone_data'] );
+		if ( isset( $response->code ) && 201 !== (int) $response->code ) {
 			return false;
 		}
 
